4f38e52c2db4577c3fb9aa55a64328cc4e8e8ac7
Anton1919 <bf3kenzo@mail.ru>
2025-05-21 15:44:01 +0300

    decomposition of table

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,7 @@
-import s from './App.module.scss';
-import { format, getDaysInMonth } from 'date-fns';
 import { useEffect, useRef } from 'react';
-
-const now = new Date();
-const currentYear = now.getFullYear();
-const currentMonth = now.getMonth();
-const currentDate = now.getDate();
-
-const getMonthDays = (year: number, month: number) => {
-  const daysInMonth = getDaysInMonth(new Date(year, month, 1));
-  const shortFormat = 'EEE';
-  return Array.from({ length: daysInMonth }, (_, i) => {
-    const date = new Date(year, month, i + 1);
-    return {
-      dayNum: i + 1,
-      dayTitle: format(date, shortFormat),
-    };
-  });
-};
-
-const tasks = [
-  { id: '1', title: 'Development' },
-  { id: '2', title: 'Testing' },
-  { id: '3', title: 'Planning' },
-];
+import { Table } from '@/widgets/Table';
 
 export function App() {
-  const days = getMonthDays(currentYear, currentMonth);
   const todayRef = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
@@ -37,54 +12,10 @@ export function App() {
       });
     }
   }, []);
+
   return (
-    <div className={s.tableWrapper}>
-      {/* Header */}
-      <div className={s.tableRow}>
-        <div className={s.taskTitleSticky}>Task</div>
-        <div className={s.daysRow}>
-          {days.map(({ dayNum, dayTitle }) => {
-            console.log(currentDate === dayNum);
-            return (
-              <div
-                className={s.dayCell}
-                key={dayNum}
-                ref={currentDate === dayNum ? todayRef : null}
-              >
-                <span className={s.dayNum}>{dayNum}</span>
-                <span className={s.dayTitle}>{dayTitle}</span>
-              </div>
-            );
-          })}
-        </div>
-        <div className={s.totalSticky}>Total</div>
-      </div>
-      {/* Task rows */}
-      {tasks.map((task) => (
-        <div className={s.tableRow} key={task.id}>
-          <div className={s.taskTitleSticky}>{task.title}</div>
-          <div className={s.daysRow}>
-            {days.map(({ dayNum }) => (
-              <div className={s.dayCell} key={dayNum}>
-                <span>-</span>
-              </div>
-            ))}
-          </div>
-          <div className={s.totalSticky}>-</div>
-        </div>
-      ))}
-      {/* Total row */}
-      <div className={s.tableRow}>
-        <div className={s.taskTitleSticky}>Total</div>
-        <div className={s.daysRow}>
-          {days.map(({ dayNum }) => (
-            <div className={s.dayCellTotal} key={dayNum}>
-              <span>0</span>
-            </div>
-          ))}
-        </div>
-        <div className={s.totalSticky}>0</div>
-      </div>
-    </div>
+    <>
+      <Table />
+    </>
   );
 }
---
+++ src/widgets/Table/ui/Table.tsx
@@ -0,0 +1,105 @@
+import s from './Table.module.scss';
+import { format, getDaysInMonth } from 'date-fns';
+import { useEffect, useRef } from 'react';
+import clsx from 'clsx';
+
+const tasks = [
+  { id: '1', title: 'Development' },
+  { id: '2', title: 'Testing' },
+  { id: '3', title: 'Planning' },
+];
+
+const now = new Date();
+const currentYear = now.getFullYear();
+const currentMonth = now.getMonth();
+
+const getMonthDays = (year: number, month: number) => {
+  const daysInMonth = getDaysInMonth(new Date(year, month, 1));
+  const shortFormat = 'EEE';
+  return Array.from({ length: daysInMonth }, (_, i) => {
+    const date = new Date(year, month, i + 1);
+    return {
+      dayNum: i + 1,
+      dayTitle: format(date, shortFormat),
+    };
+  });
+};
+
+export function Table() {
+  const days = getMonthDays(currentYear, currentMonth);
+  const currentDate = now.getDate();
+  const totalRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (totalRef && totalRef.current) {
+      totalRef.current.scrollIntoView({ behavior: 'smooth', inline: 'center' });
+    }
+  }, []);
+
+  const tableTitleStyles = (isBold: boolean = true) => {
+    return clsx(s['table__title'], {
+      [s['table__title--bold']]: isBold,
+    });
+  };
+
+  const tableDaysStyles = (isGreyBg: boolean = true) => {
+    return clsx(s['table__days'], {
+      [s['table__days--grey']]: isGreyBg,
+    });
+  };
+
+  const tableTotalStickyStyles = (isGreyBg: boolean = true) => {
+    return clsx(s['table__total-sticky'], {
+      [s['table__total-sticky--grey']]: isGreyBg,
+    });
+  };
+
+  return (
+    <div className={s['table']}>
+      <div className={s['table__row']}>
+        <h3 className={tableTitleStyles()}>Task</h3>
+        <div className={tableDaysStyles()}>
+          {days.map(({ dayNum, dayTitle }) => {
+            return (
+              <div
+                className={s['table__day-cell']}
+                key={dayNum}
+                ref={dayNum === currentDate ? totalRef : null}
+              >
+                <span className={s['table__day-cell-num']}>{dayNum}</span>
+                <span className={s['table__day-cell-title']}>{dayTitle}</span>
+              </div>
+            );
+          })}
+        </div>
+        <h3 className={tableTotalStickyStyles(false)}>Total</h3>
+      </div>
+
+      {tasks.map((task) => (
+        <div className={s['table__row']} key={task.id}>
+          <div className={tableTitleStyles(false)}>{task.title}</div>
+          <div className={tableDaysStyles(false)}>
+            {days.map(({ dayNum }) => (
+              <div className={s['table__day-cell']} key={dayNum}>
+                <span>-</span>
+              </div>
+            ))}
+          </div>
+          <h3 className={tableTotalStickyStyles()}>0</h3>
+        </div>
+      ))}
+
+      <div className={s['table__row']}>
+        <div className={tableTitleStyles()}>Total</div>
+        <div className={tableDaysStyles()}>
+          {days.map(({ dayNum }) => (
+            <div className={s['table__day-cell']} key={dayNum}>
+              <span className={s['table__day-cell-num']}>0</span>
+            </div>
+          ))}
+        </div>
+        <h3 className={tableTotalStickyStyles()}>0</h3>
+      </div>
+    </div>
+  );
+}
